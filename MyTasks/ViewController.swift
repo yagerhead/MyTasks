0abe84ab648c7ef5511b9d6dc8a96f136e0d6515
Manu Safarov <manurockyflacko@gmail.com>
2021-05-15 16:30:51 +0300

    Initial Commit

---
+++ MyTasks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTasks
+//
+//  Created by Manu Safarov on 15.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
